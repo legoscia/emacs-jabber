cbe5fbdfaea1ceb456cdcd4c3ba794ec8f6cac61
Magnus Henoch <mange@freemail.hu>
2005-05-31 21:52:13 +0000

    Revision: mange@freemail.hu--2005/emacs-jabber--cvs-head--0--patch-89 Creator:  Magnus Henoch <mange@freemail.hu>
    Check jabber-events-confirm-composing in jabber-events-after-change
    
    Before this change, if you opened a chat buffer with composing
    notification active, and set jabber-events-confirm-composing to nil, this
    buffer would still send composing events.

--- jabber-events.el
+++ jabber-events.el
@@ -136,7 +136,8 @@ and it hasn't been sent before."
 
 (defun jabber-events-after-change ()
   (let ((composing-now (not (eq (point-max) jabber-point-insert))))
-    (when (and jabber-chatting-with
+    (when (and jabber-events-confirm-composing
+	       jabber-chatting-with
 	       (not (eq composing-now jabber-events-composing-sent)))
       (jabber-send-sexp 
        `(message 
