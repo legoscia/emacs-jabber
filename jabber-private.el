2ca8705d12fa6ffd15bd1ba917d0bc01fc2994b9
Magnus Henoch <mange@freemail.hu>
2007-02-12 20:48:49 +0000

    Revision: mange@freemail.hu--2005/emacs-jabber--cvs-head--0--patch-296 Creator:  Magnus Henoch <mange@freemail.hu>
    Multiaccountify jabber-private

--- jabber-private.el
+++ jabber-private.el
@@ -19,32 +19,36 @@
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-(defun jabber-private-get (node-name namespace success-callback error-callback)
+(defun jabber-private-get (jc node-name namespace success-callback error-callback)
   "Retrieve an item from private XML storage.
 The item to retrieve is identified by NODE-NAME (a symbol) and
 NAMESPACE (a string).
-On success, SUCCESS-CALLBACK is called with the retrieved XML fragment.
-On error, ERROR-CALLBACK is called with the entire IQ result."
-  (jabber-send-iq nil "get"
+
+On success, SUCCESS-CALLBACK is called with JC and the retrieved
+XML fragment.
+
+On error, ERROR-CALLBACK is called with JC and the entire IQ
+result."
+  (jabber-send-iq jc nil "get"
 		  `(query ((xmlns . "jabber:iq:private"))
 			  (,node-name ((xmlns . ,namespace))))
 		  #'jabber-private-get-1 success-callback
-		  #'(lambda (xml-data error-callback)
-		      (funcall error-callback xml-data))
+		  #'(lambda (jc xml-data error-callback)
+		      (funcall error-callback jc xml-data))
 		  error-callback))
 
-(defun jabber-private-get-1 (xml-data success-callback)
-  (funcall success-callback
+(defun jabber-private-get-1 (jc xml-data success-callback)
+  (funcall success-callback jc
 	   (car (jabber-xml-node-children
 		 (jabber-iq-query xml-data)))))
 
-(defun jabber-private-set (fragment &optional 
-				    success-callback success-closure-data
-				    error-callback error-closure-data)
+(defun jabber-private-set (jc fragment &optional 
+			      success-callback success-closure-data
+			      error-callback error-closure-data)
   "Store FRAGMENT in private XML storage.
 SUCCESS-CALLBACK, SUCCESS-CLOSURE-DATA, ERROR-CALLBACK and
 ERROR-CLOSURE-DATA are used as in `jabber-send-iq'."
-  (jabber-send-iq nil "set"
+  (jabber-send-iq jc nil "set"
 		  `(query ((xmlns . "jabber:iq:private"))
 			  ,fragment)
 		  success-callback success-closure-data
