77697cdfcf2d37c07ccbc704bf9c1c5ebef9782b
Magnus Henoch <mange@freemail.hu>
2007-04-15 23:53:09 +0000

    Revision: mange@freemail.hu--2005/emacs-jabber--cvs-head--0--patch-344 Creator:  Magnus Henoch <mange@freemail.hu>
    Bind jabber-activity-switch-to to C-x C-j C-l

--- jabber-keymap.el
+++ jabber-keymap.el
@@ -1,7 +1,7 @@
 ;; jabber-keymap.el - common keymap for many modes
 
+;; Copyright (C) 2003, 2004, 2007 - Magnus Henoch - mange@freemail.hu
 ;; Copyright (C) 2002, 2003, 2004 - tom berger - object@intelectronica.net
-;; Copyright (C) 2003, 2004 - Magnus Henoch - mange@freemail.hu
 
 ;; This file is a part of jabber.el.
 
@@ -45,6 +45,7 @@
     (define-key map "\C-d" 'jabber-disconnect)
     (define-key map "\C-r" 'jabber-switch-to-roster-buffer)
     (define-key map "\C-j" 'jabber-chat-with)
+    (define-key map "\C-l" 'jabber-activity-switch-to)
     (define-key map "\C-a" 'jabber-send-away-presence)
     (define-key map "\C-o" 'jabber-send-default-presence)
     (define-key map "\C-x" 'jabber-send-xa-presence)
