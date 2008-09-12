10ff080b791e8f4f2d02023df531126f8655fb81
Magnus Henoch <mange@freemail.hu>
2008-09-12 20:24:45 +0000

    Revision: mange@freemail.hu--2005/emacs-jabber--cvs-head--0--patch-574 Creator:  Magnus Henoch <mange@freemail.hu>
    Don't use goto-line in jabber-display-roster, for it sets the mark

--- jabber-roster.el
+++ jabber-roster.el
@@ -396,7 +396,11 @@ H        Toggle displaying this text
 	  (dolist (hook '(jabber-info-message-hooks jabber-alert-info-message-hooks))
 	    (run-hook-with-args hook 'roster (current-buffer) (funcall jabber-alert-info-message-function 'roster (current-buffer)))))
       (when current-line
-	(goto-line current-line)
+	;; Go back to previous line - don't use goto-line, since it
+	;; sets the mark.
+	(goto-char (point-min))
+	(forward-line (1- current-line))
+	;; ...and go back to previous column
 	(move-to-column current-column)))))
 
 (defun jabber-display-roster-entry (jc buddy)
