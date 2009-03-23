604748cc77c0569a48a963b1eaca9bf4d6d85d92
Evgenii Terechkov <evg@altlinux.org>
2009-03-23 22:55:34 +0700

    Clenup before merge with upstream

--- emacs-jabber/jabber-autoloads.stub
+++
@@ -1,12 +0,0 @@
-;;; jabber-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-(provide 'jabber-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; jabber-autoloads.el ends here
--- emacs-jabber/tests/load-all.el
+++
@@ -1,8 +0,0 @@
-;; Test that all files can be loaded
-
-(let* ((default-directory (expand-file-name (getenv "top_builddir")))
-       (elc-files (file-expand-wildcards "*.elc" t)))
-  (dolist (f elc-files)
-    (load f nil t)))
-
-;; arch-tag: 509c4808-2e92-11dd-9c8c-000a95c2fcd0
