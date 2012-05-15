191cc5c3e0fdb86fa9b775c96c93f193018c5fac
Nicolas Semrau <nicolas.semrau@googlemail.com>
2012-05-15 19:50:38 +0200

    Fix missing whitespace

--- jabber-chat.el
+++ jabber-chat.el
@@ -639,7 +639,7 @@ If DONT-PRINT-NICK-P is true, don't include nickname."
 With a prefix argument, open buffer in other window.
 Returns the chat buffer."
   (interactive (let ((jid
-		      (jabber-read-jid-completing "chat with:"))
+		      (jabber-read-jid-completing "Chat with: "))
 		     (account
 		      (jabber-read-account)))
 		 (list 
