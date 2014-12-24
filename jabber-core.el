3f0c7c842fe0a2127e265fa2b95be0f19c1f860b
Magnus Henoch <magnus.henoch@gmail.com>
2014-12-24 00:15:50 +0000

    Handle server not offering session establishment
    Session establishment was removed in RFCs 6120 and 6121.  Some servers
    still offer it for backwards compatibility, but some don't.  Let's be
    compatible with the latter.

--- jabber-core.el
+++ jabber-core.el
@@ -640,8 +640,7 @@ With double prefix argument, specify more connection details."
 	((eq (jabber-xml-node-name stanza) 'features)
 	 ;; Record stream features, discarding earlier data:
 	 (setq state-data (plist-put state-data :stream-features stanza))
-	 (if (and (jabber-xml-get-children stanza 'bind)
-		  (jabber-xml-get-children stanza 'session))
+	 (if (jabber-xml-get-children stanza 'bind)
 	     (labels
 		 ((handle-bind 
 		   (jc xml-data success)
@@ -674,18 +673,23 @@ With double prefix argument, specify more connection details."
        (plist-put state-data :server (jabber-jid-server jid))
        (plist-put state-data :resource (jabber-jid-resource jid)))
 
-     ;; Been there, done that.  Time to establish a session.
-     (labels 
-	 ((handle-session
-	   (jc xml-data success)
-	   (fsm-send jc (list
-			 (if success :session-success :session-failure)
-			 xml-data))))
-       (jabber-send-iq fsm nil "set"
-		       '(session ((xmlns . "urn:ietf:params:xml:ns:xmpp-session")))
-		       #'handle-session t
-		       #'handle-session nil)
-       (list :bind state-data)))
+     ;; If the server follows the older RFCs 3920 and 3921, it may
+     ;; offer session initiation here.  If it follows RFCs 6120 and
+     ;; 6121, it might not offer it, and we should just skip it.
+     (if (jabber-xml-get-children (plist-get state-data :stream-features) 'session)
+	 (labels
+	     ((handle-session
+	       (jc xml-data success)
+	       (fsm-send jc (list
+			     (if success :session-success :session-failure)
+			     xml-data))))
+	   (jabber-send-iq fsm nil "set"
+			   '(session ((xmlns . "urn:ietf:params:xml:ns:xmpp-session")))
+			   #'handle-session t
+			   #'handle-session nil)
+	   (list :bind state-data))
+       ;; Session establishment not offered - assume not necessary.
+       (list :session-established state-data)))
 
     (:session-success
      ;; We have a session
