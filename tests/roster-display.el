e2c8fc46be5353bbaf7804a8241705d3ab9d6b34
Magnus Henoch <magnus.henoch@gmail.com>
2015-04-01 19:58:27 +0100

    Improve roster display test
    Check roster buffer after our presence state has been updated.
    Point out where the mismatch is when a test fails.
    Display contents of *fsm-debug* buffer if we cannot "connect".

--- tests/roster-display.el
+++ tests/roster-display.el
@@ -1,9 +1,14 @@
 (require 'jabberd)
+(require 'cl)
 
 (setq jabber-roster-show-bindings nil)
 
-;; jabber-post-connect-hooks is run after the roster has been drawn.
-(add-hook 'jabber-post-connect-hooks 'rd-check-roster-buffer)
+;; jabber-post-connect-hooks is run after the roster has been drawn
+;; for the first time - but jabber-send-presence will redraw the
+;; roster buffer after sending initial presence!  Make sure we check
+;; the roster buffer after that has happened, so that the roster
+;; buffer displays "Online" for ourselves already.
+(add-hook 'jabber-post-connect-hooks 'rd-check-roster-buffer :append)
 
 (defvar rd-roster-string nil)
 
@@ -12,25 +17,39 @@
    "Jabber roster\n"
    "__________________________________\n"
    "\n"
-   " - Offline -\n"
+   " - Online -\n"
    "romeo@montague.net\n"
    "__________________________________\n"
    "\n"
    "__________________________________\n"
    "\n"))
 
-(defun rd-check-roster-buffer (_jc)
+(defun rd-check-roster-buffer (&optional _jc)
   (with-current-buffer jabber-roster-buffer
     (let ((contents (buffer-string)))
       (set-text-properties 0 (length contents) () contents)
-      (prin1 contents)
       (setq rd-roster-string contents))))
+
+(defun rd-compare (title expected)
+  (princ title)
+  (princ "...")
+  (let ((result (mismatch rd-roster-string expected)))
+    (if (null result)
+	(princ "match\n")
+      (princ "mismatch!  Expected:\n")
+      (prin1 expected)
+      (princ "\nBut got:\n")
+      (prin1 (substring rd-roster-string 0 result))
+      (princ " ***mismatch here*** ")
+      (prin1 (substring rd-roster-string result))
+      (error "Mismatch"))))
 
 (jabberd-connect)
 
-(with-timeout (5 (error "Timeout"))
-  (while (not rd-roster-string)
+(with-timeout (5 (progn
+		   (princ (with-current-buffer "*fsm-debug*" (buffer-string)))
+		   (error "Timeout")))
+  (while (not (and rd-roster-string (equal "" *jabber-current-show*)))
     (sit-for 0.1)))
 
-(unless (equal rd-roster-string rd-expected-roster)
-  (error "Bad roster"))
+(rd-compare "Empty roster" rd-expected-roster)
